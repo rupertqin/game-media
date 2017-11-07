04005b1dba154df153b580cd6c128d32f4682cd6
rupert <admin@coigre.com>
2017-11-07 09:23:50 +0800

    add some tpl

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,8 +2,8 @@
 
 module.exports = app => {
   class HomeController extends app.Controller {
-    * index() {
-      this.ctx.body = 'hi, egg';
+    async index() {
+      this.ctx.body = 'hi, egg you do';
     }
   }
   return HomeController;
--- config/config.default.js
+++ config/config.default.js
@@ -4,10 +4,16 @@ module.exports = appInfo => {
   const config = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1509949515376_3730';
+  config.keys = appInfo.name + '_1509849515376_3730';
 
   // add your config here
   config.middleware = [];
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+    },
+  };
 
   return config;
 };
