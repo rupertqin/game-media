974d5bd1aa2bb38abb5267c491f43dc0376764c3
Qin Xiang <rupert.qing@gmail.com>
2017-11-14 11:20:11 +0800

    schedule view count

---
+++ app/schedule/update_view_count.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Subscription = require('egg').Subscription;
+
+class UpdateCache extends Subscription {
+  // 通过 schedule 属性来设置定时任务的执行间隔等配置
+  static get schedule() {
+    return {
+      interval: '1h', // 1 分钟间隔
+      type: 'worker', // 指定所有的 worker 都需要执行
+      // immediate: true,
+    };
+  }
+
+  // subscribe 是真正定时任务执行时被运行的函数
+  async subscribe() {
+    const evCount = await this.app.redis.hgetall('enjoy_view_count');
+    for (const k in evCount) {
+      await this.app.mysql.query('update promote_link set view_count=view_count+? where id = ?', [ evCount[k], k ]);
+    }
+    await this.app.redis.del('enjoy_view_count');
+  }
+}
+
+module.exports = UpdateCache;
