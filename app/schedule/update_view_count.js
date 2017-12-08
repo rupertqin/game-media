8b5a15d86d4f3674ae7802e7c534eb6202205d25
Qin Xiang <rupert.qing@gmail.com>
2017-12-08 10:10:14 +0800

    rm config/config.local.js && enable update_view_count schedule

--- app/schedule/update_view_count.js
+++ app/schedule/update_view_count.js
@@ -1,35 +1,37 @@
 'use strict';
 
 const Subscription = require('egg').Subscription;
-class UpdateCache extends Subscription {
-  static get schedule() {
-    return {
-      interval: '1h',
-      type: 'worker',
-      disable: true,
-      // immediate: true,
-    };
-  }
-
-  async subscribe() {
-    // 初始化事务
-    const conn = await this.app.mysql.beginTransaction();
-    try {
-      const evCount = await this.app.redis.hgetall('enjoy_view_count');
-      if (Object.getOwnPropertyNames(evCount).length === 0) return;
-      for (const k in evCount) {
-        // 缓存里的数值添加到数据库
-        await conn.query('update promote_link set view_count=view_count+? where id = ?', [ evCount[k], k ]);
-      }
-      await conn.commit(); // 提交事务
-    } catch (err) {
-      // error, rollback
-      await conn.rollback(); // 一定记得捕获异常后回滚事务！！
-      return;
+module.exports = app => {
+  class UpdateCache extends Subscription {
+    static get schedule() {
+      return {
+        interval: '1h',
+        type: 'worker',
+        disable: app.config.env !== 'prod',
+        // immediate: true,
+      };
     }
-    // 如果完成事务，清空缓存
-    await this.app.redis.del('enjoy_view_count');
-  }
-}
 
-module.exports = UpdateCache;
+    async subscribe() {
+      // 初始化事务
+      const conn = await this.app.mysql.beginTransaction();
+      try {
+        const evCount = await this.app.redis.hgetall('enjoy_view_count');
+        if (Object.getOwnPropertyNames(evCount).length === 0) return;
+        for (const k in evCount) {
+          // 缓存里的数值添加到数据库
+          await conn.query('update promote_link set view_count=view_count+? where id = ?', [ evCount[k], k ]);
+        }
+        await conn.commit(); // 提交事务
+      } catch (err) {
+        // error, rollback
+        await conn.rollback(); // 一定记得捕获异常后回滚事务！！
+        return;
+      }
+      // 如果完成事务，清空缓存
+      await this.app.redis.del('enjoy_view_count');
+    }
+  }
+
+  return UpdateCache
+}
